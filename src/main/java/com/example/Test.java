c52eb0b59cb05456bc37f92df13e7ac4b451cd12
Nikola Kaloyanov <n.kaloyanov@itfgroupbg.com>
2017-01-06 17:09:36 +0200

    opala

---
+++ src/main/java/com/example/Test.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by nikolakaloyanov on 06.01.17.
+ */
+@RestController
+public class Test {
+    @RequestMapping(path = "/ping", method = RequestMethod.GET)
+    public String ping() {
+        return "PONG";
+    }
+}
